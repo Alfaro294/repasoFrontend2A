4be735350f10639b124e3fe9a9cd9016e6960931
Alfaro294 <jenniferalfaro0819@gmail.com>
2026-05-14 09:41:34 -0600

    Creación de poryecto de repaso

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Nav from "./componets/Nav"
+import Contact from "./pages/Contact"
+
+function App() {
+
+  return (
+    <>
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App
---
+++ vite-project/src/pages/About.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+            <div className="max-w-2xl bg-white shadow-md rounded-lg p-6">
+            <h1 className="text-3xl font-bold text-gray-800 mb-4">About Us</h1>
+            <p className="text-gray-600 leading-relaxed">Welcome to our application! We are dedicated to providing the best experience for our users.Our team works hard to deliver high-quality features and ensure that everything runs smoothly.Thank you for choosing us!</p>
+
+            <div className="mt-6">
+                <h2 className="text-2xl font-semibold text-gray-800 mb-2">Our Mission</h2>
+                <p className="text-gray-600 leading-relaxed">Our mission is to create innovative solutions that empower individuals and businesses to achieve their goals. We strive to make technology accessible and user-friendly for everyone.</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default About;
